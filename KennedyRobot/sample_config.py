7a1d3a8682447a188185132221bc9fa1f38616b1
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 06:00:41 +0700

    Update sample_config.py

--- KennedyRobot/sample_config.py
+++ KennedyRobot/sample_config.py
@@ -3,7 +3,7 @@ import json
 import os
 
 def get_user_list(config, key):
-    with open('{}/JisooX/{}'.format(os.getcwd(), config), 'r') as json_file:
+    with open('{}/KennedyRobot/{}'.format(os.getcwd(), config), 'r') as json_file:
         return json.load(json_file)[key]
 
 
