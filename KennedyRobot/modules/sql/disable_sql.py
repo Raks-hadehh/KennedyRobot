eaa9c77fdf9cb39732e00e2144e37f78b0e161e8
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 11:48:38 +0700

    Update disable_sql.py

--- KennedyRobot/modules/sql/disable_sql.py
+++ KennedyRobot/modules/sql/disable_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from JisooX.modules.sql import SESSION, BASE
+from KennedyRobot.modules.sql import SESSION, BASE
 
 
 class Disable(BASE):
