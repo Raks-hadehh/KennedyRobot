ff111c594760dfdede617069c8346cc5116b46ec
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 14:29:40 +0700

    Update google.py

--- KennedyRobot/modules/google.py
+++ KennedyRobot/modules/google.py
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "Search 🔎"
+__mod_name__ = "Search"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
