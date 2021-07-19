abdfd7dca0441ff0ea7c3084e51145cad36311d5
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 11:48:18 +0700

    Update cust_filters_sql.py

--- KennedyRobot/modules/sql/cust_filters_sql.py
+++ KennedyRobot/modules/sql/cust_filters_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from JisooX.modules.sql import BASE, SESSION
+from KennedyRobot.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
