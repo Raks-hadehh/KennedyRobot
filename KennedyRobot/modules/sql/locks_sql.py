21fa12e3828f3d748bdedb881866b795e4857a9c
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 11:50:14 +0700

    Update locks_sql.py

--- KennedyRobot/modules/sql/locks_sql.py
+++ KennedyRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from JisooX.modules.sql import SESSION, BASE
+from KennedyRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
