278fd9cecf2c31bc2db96b39bd6c9c1ed6870784
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 11:50:37 +0700

    Update log_channel_sql.py

--- KennedyRobot/modules/sql/log_channel_sql.py
+++ KennedyRobot/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, func, distinct
 
-from JisooX.modules.sql import BASE, SESSION
+from KennedyRobot.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
