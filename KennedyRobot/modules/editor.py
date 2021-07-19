264834791c1de9bd7eff8fea493109f9f347e4b6
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 14:25:18 +0700

    Update editor.py

--- KennedyRobot/modules/editor.py
+++ KennedyRobot/modules/editor.py
@@ -450,7 +450,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await removebg_sticker(client, query.message)
 
 
-__mod_name__ = "Image Editor ✨"
+__mod_name__ = "Image Editor"
 __help__ = """
 <b> IMAGE EDITOR </b>
 JisooXRobot have some advanced image editing tools inbuilt
