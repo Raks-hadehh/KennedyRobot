75d00cb8152e5227a0b24e514190930fb7c5f136
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 11:56:18 +0700

    Update pluginhelpers.py

--- KennedyRobot/more/pluginhelpers.py
+++ KennedyRobot/more/pluginhelpers.py
@@ -13,8 +13,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from JisooX import OWNER_ID, GBAN_LOGS
-from JisooX.pyrogramee.pyrogram import pbot
+from KennedyRobot import OWNER_ID, GBAN_LOGS
+from KennedyRobot.pyrogramee.pyrogram import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
