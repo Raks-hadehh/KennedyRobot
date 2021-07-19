9f074ac5718b4086cff9b2f4541338ba5cbdf143
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 11:47:35 +0700

    Update cleaner_sql.py

--- KennedyRobot/modules/sql/cleaner_sql.py
+++ KennedyRobot/modules/sql/cleaner_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean
 
-from JisooX.modules.sql import BASE, SESSION
+from KennedyRobot.modules.sql import BASE, SESSION
 
 
 class CleanerBlueTextChatSettings(BASE):
