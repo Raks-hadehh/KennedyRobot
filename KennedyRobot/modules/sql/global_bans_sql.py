89ee2f9733992b0205e1ce8860730659dafd50c4
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 11:49:21 +0700

    Update global_bans_sql.py

--- KennedyRobot/modules/sql/global_bans_sql.py
+++ KennedyRobot/modules/sql/global_bans_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from JisooX.modules.sql import BASE, SESSION
+from KennedyRobot.modules.sql import BASE, SESSION
 
 
 class GloballyBannedUsers(BASE):
