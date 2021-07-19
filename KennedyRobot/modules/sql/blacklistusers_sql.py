209934a9c8153f8e900202045425d42967199602
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 11:47:00 +0700

    Update blacklistusers_sql.py

--- KennedyRobot/modules/sql/blacklistusers_sql.py
+++ KennedyRobot/modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from JisooX.modules.sql import BASE, SESSION
+from KennedyRobot.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
