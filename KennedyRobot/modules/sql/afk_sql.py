b66773e1087a3ad7f64fadc8c04ef98bd221a8bb
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 11:45:45 +0700

    Update afk_sql.py

--- KennedyRobot/modules/sql/afk_sql.py
+++ KennedyRobot/modules/sql/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Boolean, Integer
 
-from JisooX.modules.sql import BASE, SESSION
+from KennedyRobot.modules.sql import BASE, SESSION
 
 
 class AFK(BASE):
