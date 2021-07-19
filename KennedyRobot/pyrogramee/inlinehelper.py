fd55a2499fb9647b91a829b987e81d041f6a46d8
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-19 12:59:42 +0700

    Update inlinehelper.py

--- KennedyRobot/pyrogramee/inlinehelper.py
+++ KennedyRobot/pyrogramee/inlinehelper.py
@@ -74,7 +74,7 @@ async def alive_function(answers):
     bot_state = "Dead" if not await app.get_me() else "Alive"
     # ubot_state = 'Dead' if not await app2.get_me() else 'Alive'
     buttons.add(
-        InlineKeyboardButton("Main Bot", url="https://t.me/JisooXRobot"),
+        InlineKeyboardButton("Main Bot", url="https://t.me/KennedyXRobot"),
         InlineKeyboardButton("Go Inline!", switch_inline_query_current_chat=""),
     )
 
